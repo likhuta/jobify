aad96acbad1aed6e05619134d4068f0723e03056
likhuta <annalikhuta@gmail.com>
2022-08-19 22:44:10 +0300

    register form + initialize reducer + set global context

--- client/src/pages/Register.js
+++ client/src/pages/Register.js
@@ -1,19 +1,21 @@
 import { useState, useEffect } from "react";
 import { Logo, FormRow, Alert } from "../components";
 import Wrapper from "../assets/wrappers/RegisterPage";
+import { useAppContext } from '../context/appContext'
 
 const initialState = {
   name: "",
   email: "",
   password: "",
   isMember: true,
-  showAlert: false,
 };
 
 function Register() {
   const [values, setValues] = useState(initialState);
+  const { isLoading, showAlert, displayAlert } = useAppContext()
 
   const handleChange = (e) => {
+    setValues({...values, [e.target.name]: e.target.value} )
     console.log(e.target);
   };
 
@@ -23,14 +25,20 @@ function Register() {
 
   const onSubmit = (e) => {
     e.preventDefault();
-    console.log(e.target);
+
+    const { name, email, password, isMember } = values
+    if (!email || !password || (!isMember && !name)) {
+      displayAlert()
+      return
+    }
+    console.log(values)
   };
   return (
     <Wrapper className="full-page">
       <form className="form" onSubmit={onSubmit}>
         <Logo />
         <h3>{values.isMember ? "Login" : "Register"}</h3>
-        {values.showAlert && <Alert />}
+        {showAlert && <Alert />}
         {/* name field */}
         {!values.isMember && (
           <FormRow
