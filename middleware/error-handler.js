58b6bd8dc66284825389474835eef6a33c89d830
likhuta <annalikhuta@gmail.com>
2022-08-21 23:07:02 +0300

    use hash for user's password

--- middleware/error-handler.js
+++ middleware/error-handler.js
@@ -1,21 +1,19 @@
 import {StatusCodes} from 'http-status-codes'
 
-const errorHandlerMiddleware = (err, req, res, next) => {
-  console.log(err.message)
+const errorHandlerMiddleware = (err, req, res) => {
+  console.log(err)
   const defaultError = {
     statusCode: err.statusCode || StatusCodes.INTERNAL_SERVER_ERROR,
     msg: err.message || 'Something went wrong, try again leter'
   }
   if(err.name === "ValidationError") {
     defaultError.statusCode = StatusCodes.BAD_REQUEST
-    // defaultError.msg = err.message
     defaultError.msg= Object.values(err.errors).map((item) => item.message).join(',')
   } else if (err.code && err.code === 11000) {
     defaultError.statusCode = StatusCodes.BAD_REQUEST
     defaultError.msg = `${Object.keys(err.keyValue)} field has to be unique`
   }
 
-  // res.status(defaultError.statusCode).json({msg: err})
   res.status(defaultError.statusCode).json({msg: defaultError.msg})
 }
 
