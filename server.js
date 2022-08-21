42d48c4db00de38245c7ff215a9a518b32f13b60
likhuta <annalikhuta@gmail.com>
2022-08-21 14:16:21 +0300

    not-found and error-handler middlewares

--- server.js
+++ server.js
@@ -1,9 +1,18 @@
 import express from "express";
 
+// middleware
+import notFoundMiddleware from './middleware/not-found.js'
+import errorHandlerMiddleware from './middleware/error-handler.js'
+
 const app = express();
+
+
 app.get("/", (req, res) => {
   res.send("Welcome!");
 });
+
+app.use(notFoundMiddleware)
+app.use(errorHandlerMiddleware)
 
 const port = process.env.port || 5000;
 
