259152a1a84112ee9fbd6c857da2a0c3e193a5ba
likhuta <annalikhuta@gmail.com>
2022-08-25 11:36:06 +0300

    add react-icons

--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -1,10 +1,11 @@
 import Wrapper from '../assets/wrappers/Navbar'
+import {FaHome} from 'react-icons/fa'
 
 const Navbar = () => {
   return (
     <Wrapper>
       <h4>Navbar</h4>
-
+      <FaHome/>
     </Wrapper>
   )
 }
