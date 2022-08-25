690ad38f303ab87f84c9c6cc57215eaf1b0db7d5
likhuta <annalikhuta@gmail.com>
2022-08-25 11:30:50 +0300

    add Navbar, SmallSidebar, BigSidebar

--- client/src/components/index.js
+++ client/src/components/index.js
@@ -1,5 +1,9 @@
 import Logo from "../components/Logo";
 import FormRow from "../components/FormRow";
 import Alert from "../components/Alert"
+import Navbar from "./Navbar";
+import SmallSidebar from "./SmallSidebar";
+import BigSidebar from "./BigSidebar";
 
-export { Logo, FormRow, Alert };
+
+export { Logo, FormRow, Alert, Navbar, SmallSidebar, BigSidebar };
