23b850e41320ca6a9cd5640f8b3705467eadeb06
likhuta <annalikhuta@gmail.com>
2022-08-28 19:10:06 +0300

    add Job and JobInfo components

--- client/src/components/JobInfo.js
+++ client/src/components/JobInfo.js
@@ -1,6 +1,12 @@
-const JobInfo = () => {
+import Wrapper from '../assets/wrappers/JobInfo'
+
+const JobInfo = ({ icon, text }) => {
   return (
-    <div>JobInfo</div>
+    <Wrapper>
+      <span className='icon'>{icon}</span>
+      <span className='text'>{text}</span>
+    </Wrapper>
   )
 }
+
 export default JobInfo
--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -211,6 +211,14 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const setEditJob = (id) => {
+    console.log(`set edit job : ${id}`)
+  }
+
+  const deleteJob = (id) =>{
+    console.log(`delete : ${id}`)
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -224,6 +232,8 @@ const AppProvider = ({ children }) => {
         clearValues,
         createJob,
         getJobs,
+        setEditJob,
+        deleteJob,
       }}
     >
       {children}
