4a26870552181d1ab957d25b843de3015d22d412
likhuta <annalikhuta@gmail.com>
2022-08-28 18:13:50 +0300

    add getJobs request, add staff to reducer

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -16,6 +16,8 @@ import {
   CREATE_JOB_BEGIN,
   CREATE_JOB_SUCCESS,
   CREATE_JOB_ERROR,
+  GET_JOBS_BEGIN,
+  GET_JOBS_SUCCESS,
 } from "./actions";
 import axios from "axios";
 
@@ -184,6 +186,28 @@ const AppProvider = ({ children }) => {
     clearAlert()
   }
 
+  const getJobs = async () => {
+    let url = `/jobs`
+
+    dispatch({type: GET_JOBS_BEGIN})
+    try {
+      const { data } = await authFetch(url)
+      const { jobs, totalJobs, numOfPages } = data
+      dispatch({
+        type: GET_JOBS_SUCCESS,
+        payload: {
+          jobs,
+          totalJobs,
+          numOfPages,
+        },
+      })
+    } catch (error) {
+      // logoutUser()
+      console.log(error.response)
+    }
+    clearAlert()
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -196,6 +220,7 @@ const AppProvider = ({ children }) => {
         handleChange,
         clearValues,
         createJob,
+        getJobs,
       }}
     >
       {children}
