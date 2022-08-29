8af606629094d1db3a25e74c397ff08528f5b2fa
likhuta <annalikhuta@gmail.com>
2022-08-29 23:10:14 +0300

    monthlyApplications transform response data in aggregation chain

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -1,8 +1,9 @@
+import mongoose from "mongoose";
+import moment from 'moment'
 import Job from "../models/Job.js";
 import { StatusCodes } from "http-status-codes";
 import { BadRequestError, NotFoundError } from "../errors/index.js";
 import checkPermission from "../utils/checkPermissions.js";
-import mongoose from "mongoose";
 
 const createJob = async (req, res) => {
   // res.send('create job')
@@ -100,6 +101,12 @@ const showStats = async (req, res) => {
     { $sort: { '_id.year': -1, '_id.month': -1 } },
     { $limit: 6 },
   ]);
+
+  monthlyApplications = monthlyApplications.map((item) => {
+    const {_id:{year, month}, count} = item
+    const date = moment().month(month - 1).year(year).format("MMM Y")
+    return { date, count}
+  }).reverse()
 
   res.status(StatusCodes.OK).json({ defaultStats, monthlyApplications });
 };
