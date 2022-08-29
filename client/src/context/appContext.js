bb61a0c2eb0aff74ea2f33ee5663d00d9f657a60
likhuta <annalikhuta@gmail.com>
2022-08-29 22:33:34 +0300

    add showStats to context and reducer

--- client/src/context/appContext.js
+++ client/src/context/appContext.js
@@ -23,6 +23,8 @@ import {
   EDIT_JOB_BEGIN,
   EDIT_JOB_SUCCESS,
   EDIT_JOB_ERROR,
+  SHOW_STATS_BEGIN,
+  SHOW_STATS_SUCCESS,
 } from "./actions";
 import axios from "axios";
 
@@ -53,6 +55,8 @@ const initialState = {
   totalJobs: 0,
   numOfPages: 1,
   page: 1,
+  stats: {},
+  monthlyApplications: []
 };
 
 const AppContext = React.createContext();
@@ -252,6 +256,21 @@ const AppProvider = ({ children }) => {
     }
   }
 
+  const showStats = async () => {
+    dispatch({type: SHOW_STATS_BEGIN})
+    try {
+      const { data} = await authFetch('/jobs/stats')
+      dispatch({type: SHOW_STATS_SUCCESS, payload: {
+        stats: data.defaultStats,
+        monthlyApplications: data.monthlyApplications
+      }})
+    } catch (error) {
+      console.log(error)
+      // logoutUser()
+    }
+    clearAlert()
+  }
+
   return (
     <AppContext.Provider
       value={{
@@ -268,6 +287,7 @@ const AppProvider = ({ children }) => {
         setEditJob,
         editJob,
         deleteJob,
+        showStats,
       }}
     >
       {children}
