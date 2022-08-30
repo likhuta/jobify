08872cd7de9587733e0399f295988f3c42784dfc
likhuta <annalikhuta@gmail.com>
2022-08-30 13:03:24 +0300

    remove double render

--- client/src/index.js
+++ client/src/index.js
@@ -1,15 +1,15 @@
 import React from "react";
-import ReactDOM from "react-dom/client";
+import ReactDOM from "react-dom";
 import "normalize.css";
 import "./index.css";
 import App from "./App";
 import { AppProvider } from "./context/appContext";
 
-const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(
+ReactDOM.render(
   <React.StrictMode>
     <AppProvider>
       <App />
     </AppProvider>
-  </React.StrictMode>
-);
+  </React.StrictMode>,
+  document.getElementById('root')
+)
