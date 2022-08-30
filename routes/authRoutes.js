a466c95e48a8832257977146c111ceae5f3aaea3
likhuta <annalikhuta@gmail.com>
2022-08-30 18:09:13 +0300

    add Procfile for deployment on Heroku

--- routes/authRoutes.js
+++ routes/authRoutes.js
@@ -1,12 +1,19 @@
 import express from 'express'
 import {register, login, updateUser} from '../controllers/authController.js'
 import authenticateUser from '../middleware/auth.js'
+import rateLimiter from 'express-rate-limit'
 
 const router = express.Router()
 
+const apiLimiter = rateLimiter({
+  windowMs: 15 * 60 * 1000, // 15 minutes
+  max: 10,
+  message: 'Too many requests from this IP, please try again after 15 minutes',
+})
+
 // public
-router.route('/register').post(register)
-router.route('/login').post(login)
+router.route('/register').post(apiLimiter, register)
+router.route('/login').post(apiLimiter, login)
 
 // private
 router.route('/updateUser').patch(authenticateUser, updateUser)
