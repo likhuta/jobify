14a4c22b50d16db8622edd9903331496ade3495f
likhuta <annalikhuta@gmail.com>
2022-08-30 13:45:41 +0300

    set limit, skip, numOfPages on the server

--- controllers/jobsController.js
+++ controllers/jobsController.js
@@ -33,29 +33,38 @@ const getAllJobs = async (req, res) => {
   if (search) {
     queryObject.position = { $regex: search, $options: "i" };
   }
-  
+
   let result = Job.find(queryObject);
 
   // sort
-  if(sort === 'latest') {
-    result = result.sort("-createdAt")
+  if (sort === "latest") {
+    result = result.sort("-createdAt");
   }
-  if(sort === 'oldest') {
-    result = result.sort("createdAt")
+  if (sort === "oldest") {
+    result = result.sort("createdAt");
   }
-  if (sort === 'a-z') {
-    result = result.sort('position')
+  if (sort === "a-z") {
+    result = result.sort("position");
   }
-  if (sort === 'z-a') {
-    result = result.sort('-position')
+  if (sort === "z-a") {
+    result = result.sort("-position");
   }
 
+  // setup pagination
+  const page = Number(req.query.page) || 1;
+  const limit = Number(req.query.limit) || 10;
+  const skip = (page - 1) * limit;
+
+  result = result.skip(skip).limit(limit);
   const jobs = await result;
+
+  const totalJobs = await Job.countDocuments(queryObject);
+  const numOfPages = Math.ceil(totalJobs / limit);
 
   res.status(StatusCodes.OK).json({
     jobs,
-    totalJobs: jobs.length,
-    numOfPages: 1,
+    totalJobs,
+    numOfPages,
   });
 };
 
