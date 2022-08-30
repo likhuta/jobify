c362c5a36825be07afaa79e1e76d23a304e75add
likhuta <annalikhuta@gmail.com>
2022-08-30 10:42:44 +0300

    init Bar and Area charts

--- client/src/components/ChartsContainer.js
+++ client/src/components/ChartsContainer.js
@@ -1,6 +1,21 @@
+import { useState } from 'react'
+import BarChart from './BarChart'
+import AreaChart from './AreaChart'
+import { useAppContext } from '../context/appContext'
+import Wrapper from '../assets/wrappers/ChartsContainer'
+
 const ChartsContainer = () => {
+  const [barChart, setBarChart] = useState(true)
+  const { monthlyApplications: data } = useAppContext()
   return (
-    <h1>ChartsContainer</h1>
+    <Wrapper>
+    <h4>Monthly Applications</h4>
+
+    <button type='button' onClick={() => setBarChart(!barChart)}>
+      {barChart ? 'AreaChart' : 'BarChart'}
+    </button>
+    {barChart ? <BarChart data={data} /> : <AreaChart data={data} />}
+  </Wrapper>
   )
 }
 export default ChartsContainer
